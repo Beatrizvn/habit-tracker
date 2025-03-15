6242c574ba5798730198cb3e59b839ddbfc14d32
Beatriz Nascimento <73648297+Beatrizvn@users.noreply.github.com>
2025-03-15 12:14:12 -0300

    run reset-project

--- habit-tracker/app/(tabs)/_layout.tsx
+++ habit-tracker/app/(tabs)/_layout.tsx
@@ -1,52 +1,12 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Platform, Text, View, ViewComponent,  } from 'react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="addHabitScreen"
-        options={{
-          title: 'Add Habit',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="add" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="habitDetailScreen"
-        options={{
-          title: 'Habits Details',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="list-status" color={color} />,
-        }}
-      />
-    </Tabs>
+    <View>
+      <Text>TAB</Text>
+    </View>
   );
 }
